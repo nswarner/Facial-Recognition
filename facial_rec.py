510db32881394bfb21dcd5b8594c949540c7d04e
Nicholas Warner <nswarner2@gmail.com>
2016-09-03 18:52:27 -0700

    Added facial recognition code, training database, windows prep file
    Code retrieved from bikz.05@gmail.com, http://hanzratech.in/2015/02/03/face-recognition-using-opencv.html
    Next steps are to add a personalized training set, add the RaspberryPi's Camera as the new source comparison, and create some loading and saving code for the recognizer (so it's not rebuilding it's facial database each load).

--- facial_rec.py
+++ facial_rec.py
@@ -2,11 +2,79 @@ import cv2, os
 import numpy as np
 from PIL import Image
 
-hCascadeDir = "N:\\Python27\\haarcascades\\"
-cascadePath = hCascadeDir + "haarcascade_frontalface_default.xml"
+'''
+    Currently, all code has been provided by Bikramjot Singh Hanzra
+    Contact: bikz.05@gmail.com
+    URL: http://hanzratech.in/2015/02/03/face-recognition-using-opencv.html
+
+    Todo:
+     1. Download a personal training dataset from FB (~15-20 images per person)
+        - Setup as training_set folder
+     2. Reorganize the code to read the new training set (i.e. no .sad, .glasses, etc)
+     3. Incorporate RPi's camera module to constantly scan for faces
+     4. If recognized face, load personalized document (browser?)
+'''
+
+import cv2, os
+import numpy as np
+from PIL import Image
+
+# For face detection we will use the Haar Cascade provided by OpenCV.
+cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
+# For face recognition we will the the LBPH Face Recognizer
 recognizer = cv2.createLBPHFaceRecognizer()
 
+def get_images_and_labels(path):
+    # Append all the absolute image paths in a list image_paths
+    # We will not read the image with the .sad extension in the training set
+    # Rather, we will use them to test our accuracy of the training
+    image_paths = [os.path.join(path, f) for f in os.listdir(path) if not f.endswith('.sad')]
+    # images will contains face images
+    images = []
+    # labels will contains the label that is assigned to the image
+    labels = []
+    for image_path in image_paths:
+        # Read the image and convert to grayscale
+        image_pil = Image.open(image_path).convert('L')
+        # Convert the image format into numpy array
+        image = np.array(image_pil, 'uint8')
+        # Get the label of the image
+        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+        # Detect the face in the image
+        faces = faceCascade.detectMultiScale(image)
+        # If face is detected, append the face to images and the label to labels
+        for (x, y, w, h) in faces:
+            images.append(image[y: y + h, x: x + w])
+            labels.append(nbr)
+            cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
+            cv2.waitKey(10)
+    # return the images list and labels list
+    return images, labels
 
+# Path to the Yale Dataset
+path = './yalefaces'
+# Call the get_images_and_labels function and get the face images and the
+# corresponding labels
+images, labels = get_images_and_labels(path)
+cv2.destroyAllWindows()
 
+# Perform the tranining
+recognizer.train(images, np.array(labels))
+
+# Append the images with the extension .sad into image_paths
+image_paths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.sad')]
+for image_path in image_paths:
+    predict_image_pil = Image.open(image_path).convert('L')
+    predict_image = np.array(predict_image_pil, 'uint8')
+    faces = faceCascade.detectMultiScale(predict_image)
+    for (x, y, w, h) in faces:
+        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+        nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+        if nbr_actual == nbr_predicted:
+            print "{} is Correctly Recognized with confidence {}".format(nbr_actual, conf)
+        else:
+            print "{} is Incorrect Recognized as {}".format(nbr_actual, nbr_predicted)
+        cv2.imshow("Recognizing Face", predict_image[y: y + h, x: x + w])
+        cv2.waitKey(200)
